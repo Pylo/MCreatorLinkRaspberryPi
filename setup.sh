92c1556491131cfeece12c26466391f92eeadb27
KlemenDEV <klemen.pevec@pylo.co>
2019-09-20 19:28:53 +0200

    Renamed to MCreator Link

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Run the following line to setup Minecraft Link Service on your Pi:
+# Run the following line to setup MCreator Link Service on your Pi:
 # curl -sL http://mcreator.net/linkpi | sudo bash
 
 # WARNING!!! THIS SCRIPT REQUIRES INTERNET CONNECTION!!
@@ -24,28 +24,28 @@ sudo wget http://get.pi4j.com/download/pi4j-1.2-SNAPSHOT.deb
 sudo dpkg -i pi4j-1.2-SNAPSHOT.deb
 
 echo ====================================================
-echo Downloading Minecraft Link
+echo Downloading MCreator Link
 echo ====================================================
 
 # we make install dir
-sudo mkdir /opt/minecraftlink
+sudo mkdir /opt/mcreatorlink
 
-# download Minecraft Link
-sudo wget -O /opt/minecraftlink/minecraft_link_pi.jar https://github.com/Pylo/MinecraftLinkRaspberryPi/releases/download/1.1/minecraft-link-raspberrypi.jar
+# download MCreator Link
+sudo wget -O /opt/mcreatorlink/minecraft_link_pi.jar https://github.com/Pylo/MCreatorLinkRaspberryPi/releases/download/1.1/minecraft-link-raspberrypi.jar
 
 echo ====================================================
-echo Installing Minecraft Link service
+echo Installing MCreator Link service
 echo ====================================================
 
 # create service file
-sudo su -c 'sudo cat >/etc/systemd/system/minecraftlink.service <<EOL
+sudo su -c 'sudo cat >/etc/systemd/system/mcreatorlink.service <<EOL
 [Unit]
-Description=Minecraft Link
+Description=MCreator Link
 After=network.target
  
 [Service]
 Type=simple
-WorkingDirectory=/opt/minecraftlink
+WorkingDirectory=/opt/mcreatorlink
 ExecStart=/bin/bash -c "sudo java -cp '.:minecraft_link_pi.jar:/opt/pi4j/lib/*' net.mcreator.minecraft.link.raspberrypi.Service"
 Restart=always
 User=root
@@ -55,24 +55,24 @@ WantedBy=multi-user.target
 EOL'
 
 # activate the service
-sudo chmod 644 /etc/systemd/system/minecraftlink.service
+sudo chmod 644 /etc/systemd/system/mcreatorlink.service
 sudo systemctl daemon-reload
-sudo systemctl enable minecraftlink.service
-sudo systemctl start minecraftlink.service
+sudo systemctl enable mcreatorlink.service
+sudo systemctl start mcreatorlink.service
 
 # wait for the service to start
 
 sleep 5
 
 # check the status
-sudo systemctl status minecraftlink.service
+sudo systemctl status mcreatorlink.service
 
 echo ====================================================
-echo Minecraft Link was installed. Service is now running
+echo MCreator Link was installed. Service is now running
 echo and will be running by default in the startup.
 echo
 echo To check the service status, use this command:
 echo
-echo sudo systemctl status minecraftlink.service
+echo sudo systemctl status mcreatorlink.service
 echo
 echo ====================================================
